7df255e8ac2a5df76228e74e82d6a9440052032d
Rafal Halama <rafau.halama@gmail.com>
2023-08-07 18:54:24 +0200

    manga interface created

---
+++ frontend/src/app/common/manga.ts
@@ -0,0 +1,10 @@
+export interface Manga {
+  id: string;
+  title: string;
+  category: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
