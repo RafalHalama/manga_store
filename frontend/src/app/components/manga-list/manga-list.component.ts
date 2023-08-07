6bf993decb227a2778673e599870bfad6d7b2848
Rafal Halama <rafau.halama@gmail.com>
2023-08-07 18:54:37 +0200

    mangaList component created

--- frontend/src/app/components/manga-list/manga-list.component.ts
+++ frontend/src/app/components/manga-list/manga-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Manga } from 'src/app/common/manga';
+import { MangaService } from 'src/app/services/manga.service';
 
 @Component({
   selector: 'app-manga-list',
   templateUrl: './manga-list.component.html',
-  styleUrls: ['./manga-list.component.css']
+  styleUrls: ['./manga-list.component.css'],
 })
-export class MangaListComponent {
+export class MangaListComponent implements OnInit {
+  mangaList: Manga[] = [];
 
+  constructor(private mangaService: MangaService) {}
+
+  ngOnInit() {
+    this.listManga();
+  }
+
+  listManga() {
+    this.mangaService.getMangaList().subscribe((data) => {
+      this.mangaList = data;
+    });
+  }
 }
