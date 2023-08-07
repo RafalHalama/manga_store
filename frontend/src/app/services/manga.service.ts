fd4341da64067091c898a354eb86ae611bc460bf
Rafal Halama <rafau.halama@gmail.com>
2023-08-07 18:54:50 +0200

    manga service created

--- frontend/src/app/services/manga.service.ts
+++ frontend/src/app/services/manga.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Manga } from '../common/manga';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MangaService {
+  private baseUrl = `${environment.apiUrl}` + 'manga';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getMangaList(): Observable<Manga[]> {
+    return this.httpClient.get<Manga[]>(this.baseUrl);
+  }
 }
