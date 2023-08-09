337fb2463633b846f8cb0febbc7ba8afcb31b181
Rafal Halama <rafau.halama@gmail.com>
2023-08-09 21:17:25 +0200

    auth service, cart service created, manga-service upgraded

--- frontend/src/app/services/manga.service.ts
+++ frontend/src/app/services/manga.service.ts
@@ -3,16 +3,25 @@ import { HttpClient } from '@angular/common/http';
 import { Manga } from '../common/manga';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Category } from '../common/category';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MangaService {
-  private baseUrl = `${environment.apiUrl}` + 'manga';
+  private baseUrl = `${environment.apiUrl}`;
 
   constructor(private httpClient: HttpClient) {}
 
   getMangaList(): Observable<Manga[]> {
-    return this.httpClient.get<Manga[]>(this.baseUrl);
+    return this.httpClient.get<Manga[]>(this.baseUrl + 'manga');
+  }
+
+  getMangaById(id: number): Observable<Manga> {
+    return this.httpClient.get<Manga>(this.baseUrl + 'manga/' + id);
+  }
+
+  getCategories(): Observable<Category[]> {
+    return this.httpClient.get<Category[]>(this.baseUrl + 'categories');
   }
 }
